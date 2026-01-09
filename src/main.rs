8b4e729c7f6aebebb7d905f6fd0198aac8905057
Shaun Ho <sefho98@gmail.com>
2026-01-09 18:22:20 +0000

    Add parser as a task

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use flights::cli::Cli;
 use flights::config::ApplicationConfig;
 use flights::ingestor::Ingestor;
 use flights::logging::setup_logging;
+use flights::parser::AircraftParser;
+use flights::parser::types::Aircraft;
 use flights::thread_manager::ThreadManager;
 use log::info;
 
 fn main() {
-    setup_logging(log::LevelFilter::Debug);
+    setup_logging(log::LevelFilter::Info);
     let cli = Cli::parse();
     let application_config = ApplicationConfig::construct_from_path(&cli.config_file)
         .unwrap_or_else(|e| {
@@ -16,20 +18,30 @@ fn main() {
         });
     info!("Main: Application started.");
 
-    let (messages_sender, _messages_receiver): (
+    let (messages_sender, messages_receiver): (
         crossbeam_channel::Sender<String>,
         crossbeam_channel::Receiver<String>,
+    ) = crossbeam_channel::unbounded();
+
+    let (aircraft_data_sender, _aircraft_data_receiver): (
+        crossbeam_channel::Sender<Aircraft>,
+        crossbeam_channel::Receiver<Aircraft>,
     ) = crossbeam_channel::unbounded();
 
     let ingestor = Ingestor::new(&application_config.glidernet, messages_sender)
         .map_err(|e| log::error!("Error constructing ingestor: {e}"))
         .unwrap();
 
+    let parser = AircraftParser::new(messages_receiver, aircraft_data_sender);
+
     let mut thread_manager = ThreadManager::new();
-    let ingestor_task_id = thread_manager.add_task(ingestor, std::time::Duration::from_micros(50));
+    let _ingestor_task_id = thread_manager.add_task(ingestor, std::time::Duration::from_micros(50));
+    let aircraft_parser_task_id =
+        thread_manager.add_task(parser, std::time::Duration::from_micros(50));
 
     std::thread::sleep(std::time::Duration::from_secs(5));
     thread_manager.stop_all_tasks();
-    thread_manager.wait_on_task_finish(ingestor_task_id);
+    thread_manager.wait_on_task_finish(aircraft_parser_task_id);
+
     info!("Main: Program finished.");
 }
