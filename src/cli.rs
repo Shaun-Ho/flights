934cdc3b0cd8df6d81044cc0bb4b210d8194399e
Shaun Ho <112622910+Shaun-Ho@users.noreply.github.com>
2026-01-09 20:27:43 +0000

    Set logging level to cli option (#7)
    This PR sets the logging level as a CLI option via the `--logging-level`
    flag

--- src/cli.rs
+++ src/cli.rs
@@ -6,8 +6,8 @@ pub struct Cli {
     #[arg(long, default_value_t = -1)]
     pub duration: i32,
 
-    #[arg(short, long, default_value_t = false)]
-    pub debug: bool,
+    #[arg(short, long, default_value_t = log::LevelFilter::Info)]
+    pub logging_level: log::LevelFilter,
 
     #[arg(long)]
     pub config_file: std::path::PathBuf,
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ use flights::thread_manager::ThreadManager;
 use log::info;
 
 fn main() {
-    setup_logging(log::LevelFilter::Info);
     let cli = Cli::parse();
     let application_config = ApplicationConfig::construct_from_path(&cli.config_file)
         .unwrap_or_else(|e| {
             log::error!("{e}");
             panic!("Config error. Exiting.")
         });
+
+    setup_logging(cli.logging_level);
     info!("Main: Application started.");
 
     let (messages_sender, messages_receiver): (
@@ -35,7 +36,7 @@ fn main() {
     let parser = AircraftParser::new(messages_receiver, aircraft_data_sender);
 
     let mut thread_manager = ThreadManager::new();
-    let _ingestor_task_id = thread_manager.add_task(ingestor, std::time::Duration::from_micros(50));
+    thread_manager.add_task(ingestor, std::time::Duration::from_micros(50));
     let aircraft_parser_task_id =
         thread_manager.add_task(parser, std::time::Duration::from_micros(50));
 
