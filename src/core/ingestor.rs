c52a2d2d89ea8d00b33605d58fca25e5009b5beb
Shaun Ho <sefho98@gmail.com>
2026-02-02 23:17:10 +0000

    Add option for ingestor to log to disk

--- src/core/ingestor.rs
+++ src/core/ingestor.rs
@@ -1,6 +1,7 @@
 pub mod config;
 pub mod error;
 
+use crate::cli::IngestorConfig;
 use crate::core::ingestor::config::GliderNetConfig;
 use crate::core::thread_manager::SteppableTask;
 
@@ -11,23 +12,44 @@ use std::io::{BufRead, Write};
 pub struct Ingestor {
     reader: std::io::BufReader<std::net::TcpStream>,
     sender: crossbeam_channel::Sender<String>,
+    output_writer: Option<std::io::BufWriter<std::fs::File>>,
 }
 impl Ingestor {
     pub fn new(
-        config: &GliderNetConfig,
+        glidernet: &GliderNetConfig,
         sender: crossbeam_channel::Sender<String>,
+        config: IngestorConfig,
     ) -> Result<Self, std::io::Error> {
         let login = format!(
             "user N0CALL pass -1 vers AirspaceRadar 0.1.0 filter {0} \r\n",
-            config.filter
+            glidernet.filter
         );
         log::info!("Connecting to TCP stream.");
         let mut stream =
-            std::net::TcpStream::connect(format!("{0}:{1}", config.host, config.port))?;
+            std::net::TcpStream::connect(format!("{0}:{1}", glidernet.host, glidernet.port))?;
         stream.write_all(login.as_bytes())?;
         log::info!("Connection successful.");
         let reader = std::io::BufReader::new(stream);
-        Ok(Ingestor { reader, sender })
+
+        let writer = config
+            .log_input_data_stream
+            .map(|output_path| {
+                if output_path.exists() {
+                    std::fs::File::options()
+                        .append(true)
+                        .open(&output_path)
+                        .map(std::io::BufWriter::new)
+                } else {
+                    std::fs::File::create(&output_path).map(std::io::BufWriter::new)
+                }
+            })
+            .transpose()?;
+
+        Ok(Ingestor {
+            reader,
+            sender,
+            output_writer: writer,
+        })
     }
 }
 
@@ -40,6 +62,11 @@ impl SteppableTask for Ingestor {
             return true;
         };
 
+        if let Some(output_writer) = &mut self.output_writer {
+            let _ = write!(output_writer, "{line_buffer}")
+                .map_err(|error| log::error!("Failed to log to disk: {error}"));
+        }
+        
         if bytes_read == 0 {
             log::error!("End of TCP stream");
             return false;
