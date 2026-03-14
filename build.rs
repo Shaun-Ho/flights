f6bdd0bbcef06c935748568b59adf511e69056bf
Shaun Ho <sefho98@gmail.com>
2026-03-14 20:14:19 +0000

    Add build script for protobuf

---
+++ build.rs
@@ -0,0 +1,21 @@
+use glob::glob;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=src/");
+
+    let mut proto_files = Vec::new();
+
+    let paths = glob("src/protobuf/**/*.proto").expect("Failed to read glob pattern");
+
+    for entry in paths {
+        match entry {
+            Ok(path) => proto_files.push(path),
+            Err(e) => println!("cargo:warning=Glob error: {e:?}"),
+        }
+    }
+
+    prost_build::compile_protos(&proto_files, &["src/"])?;
+
+    Ok(())
+}
