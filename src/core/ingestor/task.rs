ff9f2691871d90b173dba95a5610ff599aa9055d
Shaun Ho <sefho98@gmail.com>
2026-03-20 18:33:14 +0000

    Refactor disk writing to separate module

--- src/core/ingestor/task.rs
+++ src/core/ingestor/task.rs
@@ -1,9 +1,8 @@
-use std::io::Write;
-
 use prost::Message;
 
 use crate::core::ingestor::config::GliderNetConfig;
-use crate::core::ingestor::error::{self, PacketConversionError};
+use crate::core::ingestor::disk::write_pb_aprs_packet_to_disk;
+use crate::core::ingestor::error;
 use crate::core::ingestor::protobuf::PbAprsPacket;
 use crate::core::thread_manager::SteppableTask;
 
@@ -13,31 +12,6 @@ pub struct AprsPacket {
     pub message: bytes::Bytes,
 }
 
-impl TryFrom<PbAprsPacket> for AprsPacket {
-    type Error = PacketConversionError;
-    fn try_from(packet: PbAprsPacket) -> Result<Self, Self::Error> {
-        let timestamp = packet
-            .timestamp
-            .ok_or(PacketConversionError::MissingTimestamp)?
-            .try_into()?;
-
-        Ok(Self {
-            timestamp,
-            message: packet.message,
-        })
-    }
-}
-
-impl From<AprsPacket> for PbAprsPacket {
-    fn from(packet: AprsPacket) -> Self {
-        let pb_timestamp = packet.timestamp.into();
-
-        Self {
-            timestamp: Some(pb_timestamp),
-            message: packet.message,
-        }
-    }
-}
 pub struct Ingestor {
     source: Box<dyn APRSDataSource>,
     sender: crossbeam_channel::Sender<AprsPacket>,
@@ -215,15 +189,6 @@ impl APRSDataSource for ReplaySource {
             }
         }
     }
-}
-
-pub fn write_pb_aprs_packet_to_disk(
-    writer: &mut std::io::BufWriter<std::fs::File>,
-    aprs_packet: &PbAprsPacket,
-) -> Result<(), std::io::Error> {
-    let mut buf = Vec::new();
-    let () = aprs_packet.encode_length_delimited(&mut buf)?;
-    writer.write_all(&buf)
 }
 
 fn authentication_handshake<W: std::io::Write>(
