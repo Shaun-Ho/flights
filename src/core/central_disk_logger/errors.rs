03fd31fccb1c802e17921d3bd60543b9c36beed3
Shaun Ho <sefho98@gmail.com>
2026-06-27 14:01:50 +0100

    Add logger registry

--- src/core/central_disk_logger/errors.rs
+++ src/core/central_disk_logger/errors.rs
@@ -4,15 +4,23 @@ use std::path::PathBuf;
 use crate::core::central_disk_logger::task::LoggerTaskID;
 
 #[derive(Debug, thiserror::Error)]
-pub enum CentralisedLoggerError {
-    #[error("TaskID not registered: {0}")]
-    TaskNotRegistered(LoggerTaskID),
+pub enum DiskloggerRegistryError {
+    #[error("path already registered: {0}")]
+    PathAlreadyRegisteredError(PathBuf),
+
     #[error("Unable to create logger file at path: {path}")]
     LogFileCreationError {
         path: PathBuf,
         #[source]
         source: io::Error,
     },
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum CentralDiskLoggerError {
+    #[error("TaskID not registered: {0}")]
+    TaskNotRegistered(LoggerTaskID),
+
     #[error("Unable to write data to log file: {path}")]
     WriteError {
         path: PathBuf,
--- src/core/central_disk_logger/task.rs
+++ src/core/central_disk_logger/task.rs
@@ -5,37 +5,85 @@ use std::path::PathBuf;
 
 use crate::core::central_disk_logger::errors;
 use crate::core::thread_manager::{SteppableTask, TaskState};
+use crate::ext::TryInsertExt;
 
 pub type LoggerTaskID = u8;
 
+#[derive(Debug)]
 pub struct DiskLoggerMessage {
     pub logger_id: LoggerTaskID,
     pub payload: Vec<u8>,
 }
 
+#[derive(Debug)]
+pub struct LoggerHandle {
+    #[allow(unused)]
+    logger_id: LoggerTaskID,
+    #[allow(unused)]
+    sender: crossbeam_channel::Sender<DiskLoggerMessage>,
+}
+
+#[derive(Debug)]
+pub struct DiskLoggerRegistry {
+    current_logger_id: LoggerTaskID,
+    sender: crossbeam_channel::Sender<DiskLoggerMessage>,
+    receiver: crossbeam_channel::Receiver<DiskLoggerMessage>,
+    task_to_path_mapping: HashMap<LoggerTaskID, (PathBuf, BufWriter<File>)>,
+}
+impl DiskLoggerRegistry {
+    pub fn new() -> Self {
+        let (sender, receiver) = crossbeam_channel::unbounded();
+        Self {
+            current_logger_id: 0,
+            sender,
+            receiver,
+            task_to_path_mapping: HashMap::new(),
+        }
+    }
+
+    pub fn register(
+        &mut self,
+        path: PathBuf,
+    ) -> Result<LoggerHandle, errors::DiskloggerRegistryError> {
+        let file = match File::create_new(&path) {
+            Ok(f) => f,
+            Err(err) => {
+                return Err(errors::DiskloggerRegistryError::LogFileCreationError {
+                    path,
+                    source: err,
+                });
+            }
+        };
+        let writer = BufWriter::new(file);
+
+        let logger_id = self.current_logger_id;
+        let _ = TryInsertExt::try_insert(&mut self.task_to_path_mapping, logger_id, (path, writer))
+            .map_err(|err| {
+                let (rejected_path, _rejected_writer) = err.value;
+                errors::DiskloggerRegistryError::PathAlreadyRegisteredError(rejected_path)
+            })?;
+
+        let handle = LoggerHandle {
+            logger_id,
+            sender: self.sender.clone(),
+        };
+        self.current_logger_id += 1;
+
+        Ok(handle)
+    }
+
+    pub fn build(self) -> CentralDiskLogger {
+        CentralDiskLogger {
+            id_to_path_writer_pair_mapping: self.task_to_path_mapping,
+            receiver: self.receiver,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct CentralDiskLogger {
     receiver: crossbeam_channel::Receiver<DiskLoggerMessage>,
     id_to_path_writer_pair_mapping: HashMap<LoggerTaskID, (PathBuf, BufWriter<File>)>,
-}
-impl CentralDiskLogger {
-    pub fn new(
-        id_to_path_mapping: HashMap<LoggerTaskID, PathBuf>,
-        receiver: crossbeam_channel::Receiver<DiskLoggerMessage>,
-    ) -> Result<Self, std::io::Error> {
-        let id_to_path_writer_pair_mapping = id_to_path_mapping
-            .into_iter()
-            .map(|(logger_id, path)| {
-                let file = File::create_new(path.clone())?;
-                Ok((logger_id, (path, BufWriter::new(file))))
-            })
-            .collect::<Result<HashMap<LoggerTaskID, (PathBuf, BufWriter<File>)>, std::io::Error>>(
-            )?;
-
-        Ok(Self {
-            receiver,
-            id_to_path_writer_pair_mapping,
-        })
-    }
 }
 
 impl SteppableTask for CentralDiskLogger {
@@ -45,12 +93,12 @@ impl SteppableTask for CentralDiskLogger {
                 match self
                     .id_to_path_writer_pair_mapping
                     .get_mut(&message.logger_id)
-                    .ok_or(errors::CentralisedLoggerError::TaskNotRegistered(
+                    .ok_or(errors::CentralDiskLoggerError::TaskNotRegistered(
                         message.logger_id,
                     )) {
                     Ok((path, writer)) => {
                         let _ = writer.write_all(&message.payload).map_err(|err| {
-                            let write_error = errors::CentralisedLoggerError::WriteError {
+                            let write_error = errors::CentralDiskLoggerError::WriteError {
                                 path: path.clone(),
                                 payload: message.payload,
                                 source: err,
@@ -70,122 +118,156 @@ impl SteppableTask for CentralDiskLogger {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
+    mod disk_logger_registry {
+        use super::*;
+        use std::fs;
+        #[test]
+        fn given_valid_paths_when_creating_logger_then_files_are_created() {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let file_path = temp_dir.path().join("test_log_1.bin");
 
-    #[test]
-    fn given_valid_paths_when_creating_logger_then_files_are_created() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let file_path = temp_dir.path().join("test_log_1.bin");
-        let mut mapping = HashMap::new();
-        mapping.insert(1, file_path.clone());
+            let mut registry = DiskLoggerRegistry::new();
+            let handle = registry.register(file_path.clone());
+            assert!(handle.is_ok());
+            assert!(file_path.exists());
+        }
 
-        let (_sender, receiver) = crossbeam_channel::unbounded();
+        #[test]
+        fn given_existing_file_when_creating_logger_then_returns_io_error() {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let file_path = temp_dir.path().join("already_exists.bin");
+            fs::File::create(&file_path).unwrap();
 
-        let logger = CentralDiskLogger::new(mapping, receiver);
+            let mut register = DiskLoggerRegistry::new();
+            let handle = register.register(file_path.clone());
 
-        assert!(logger.is_ok());
-        assert!(file_path.exists());
+            assert!(handle.is_err());
+        }
     }
 
-    #[test]
-    fn given_existing_file_when_creating_logger_then_returns_io_error() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let file_path = temp_dir.path().join("already_exists.bin");
-        fs::File::create(&file_path).unwrap();
+    mod central_disk_logger {
+        use super::*;
+        use std::fs;
+        #[test]
+        fn given_valid_message_when_stepped_then_writes_payload_to_file() {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let file_path = temp_dir.path().join("data_log.bin");
+            let mut mapping = HashMap::new();
+            let task_id = 42;
+            mapping.insert(
+                task_id,
+                (
+                    file_path.clone(),
+                    BufWriter::new(File::create_new(&file_path).unwrap()),
+                ),
+            );
 
-        let mut mapping = HashMap::new();
-        mapping.insert(1, file_path);
+            let (sender, receiver) = crossbeam_channel::unbounded();
+            let mut logger = CentralDiskLogger {
+                id_to_path_writer_pair_mapping: mapping,
+                receiver,
+            };
 
-        let (_sender, receiver) = crossbeam_channel::unbounded();
+            let expected_payload = b"test payload bytes".to_vec();
+            sender
+                .send(DiskLoggerMessage {
+                    logger_id: task_id,
+                    payload: expected_payload.clone(),
+                })
+                .unwrap();
 
-        let logger_result = CentralDiskLogger::new(mapping, receiver);
+            let state = logger.step();
 
-        assert!(logger_result.is_err(),);
-    }
+            assert!(matches!(state, TaskState::Running),);
 
-    #[test]
-    fn given_valid_message_when_stepped_then_writes_payload_to_file() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let file_path = temp_dir.path().join("data_log.bin");
-        let mut mapping = HashMap::new();
-        let task_id = 42;
-        mapping.insert(task_id, file_path.clone());
+            // We must drop the logger (and its BufWriter) to ensure the internal
+            // buffer flushes its contents to the actual disk before we test reading it.
+            drop(logger);
 
-        let (sender, receiver) = crossbeam_channel::unbounded();
-        let mut logger = CentralDiskLogger::new(mapping, receiver).unwrap();
+            let written_contents = fs::read(&file_path).unwrap();
+            assert_eq!(written_contents, expected_payload,);
+        }
 
-        let expected_payload = b"test payload bytes".to_vec();
-        sender
-            .send(DiskLoggerMessage {
-                logger_id: task_id,
-                payload: expected_payload.clone(),
-            })
-            .unwrap();
+        #[test]
+        fn given_empty_channel_when_stepped_then_returns_running() {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let file_path = temp_dir.path().join("empty_test.bin");
+            let mut mapping = HashMap::new();
+            mapping.insert(
+                1,
+                (
+                    file_path.clone(),
+                    BufWriter::new(File::create_new(&file_path).unwrap()),
+                ),
+            );
 
-        let state = logger.step();
+            let (_sender, receiver) = crossbeam_channel::unbounded();
+            let mut logger = CentralDiskLogger {
+                id_to_path_writer_pair_mapping: mapping,
+                receiver,
+            };
 
-        assert!(matches!(state, TaskState::Running),);
+            let state = logger.step();
 
-        // We must drop the logger (and its BufWriter) to ensure the internal
-        // buffer flushes its contents to the actual disk before we test reading it.
-        drop(logger);
+            assert!(matches!(state, TaskState::Running),);
+        }
 
-        let written_contents = fs::read(&file_path).unwrap();
-        assert_eq!(written_contents, expected_payload,);
-    }
+        #[test]
+        fn given_disconnected_channel_when_stepped_then_returns_completed() {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let file_path = temp_dir.path().join("disconnect_test.bin");
+            let mut mapping = HashMap::new();
+            mapping.insert(
+                1,
+                (
+                    file_path.clone(),
+                    BufWriter::new(File::create_new(&file_path).unwrap()),
+                ),
+            );
 
-    #[test]
-    fn given_empty_channel_when_stepped_then_returns_running() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let file_path = temp_dir.path().join("empty_test.bin");
-        let mut mapping = HashMap::new();
-        mapping.insert(1, file_path);
+            let (sender, receiver) = crossbeam_channel::unbounded();
+            let mut logger = CentralDiskLogger {
+                id_to_path_writer_pair_mapping: mapping,
+                receiver,
+            };
 
-        let (_sender, receiver) = crossbeam_channel::unbounded();
-        let mut logger = CentralDiskLogger::new(mapping, receiver).unwrap();
+            // Explicitly drop the sender to disconnect the channel
+            drop(sender);
 
-        let state = logger.step();
+            let state = logger.step();
 
-        assert!(matches!(state, TaskState::Running),);
-    }
+            assert!(matches!(state, TaskState::Completed),);
+        }
 
-    #[test]
-    fn given_disconnected_channel_when_stepped_then_returns_completed() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let file_path = temp_dir.path().join("disconnect_test.bin");
-        let mut mapping = HashMap::new();
-        mapping.insert(1, file_path);
+        #[test]
+        fn given_unregistered_logger_id_when_stepped_then_ignores_and_returns_running() {
+            let temp_dir = tempfile::tempdir().unwrap();
+            let file_path = temp_dir.path().join("unregistered_test.bin");
+            let mut mapping = HashMap::new();
+            mapping.insert(
+                1,
+                (
+                    file_path.clone(),
+                    BufWriter::new(File::create_new(&file_path).unwrap()),
+                ),
+            );
 
-        let (sender, receiver) = crossbeam_channel::unbounded();
-        let mut logger = CentralDiskLogger::new(mapping, receiver).unwrap();
+            let (sender, receiver) = crossbeam_channel::unbounded();
+            let mut logger = CentralDiskLogger {
+                id_to_path_writer_pair_mapping: mapping,
+                receiver,
+            };
 
-        // Explicitly drop the sender to disconnect the channel
-        drop(sender);
+            sender
+                .send(DiskLoggerMessage {
+                    logger_id: 99,
+                    payload: b"ghost payload".to_vec(),
+                })
+                .unwrap();
 
-        let state = logger.step();
+            let state = logger.step();
 
-        assert!(matches!(state, TaskState::Completed),);
-    }
-
-    #[test]
-    fn given_unregistered_logger_id_when_stepped_then_ignores_and_returns_running() {
-        let temp_dir = tempfile::tempdir().unwrap();
-        let file_path = temp_dir.path().join("unregistered_test.bin");
-        let mut mapping = HashMap::new();
-        mapping.insert(1, file_path);
-
-        let (sender, receiver) = crossbeam_channel::unbounded();
-        let mut logger = CentralDiskLogger::new(mapping, receiver).unwrap();
-
-        sender
-            .send(DiskLoggerMessage {
-                logger_id: 99,
-                payload: b"ghost payload".to_vec(),
-            })
-            .unwrap();
-
-        let state = logger.step();
-
-        assert!(matches!(state, TaskState::Running));
+            assert!(matches!(state, TaskState::Running));
+        }
     }
 }
