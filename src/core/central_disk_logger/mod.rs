e2efca6d09d67cd8d4dc14cf67da38ebf08cd9cf
Shaun Ho <sefho98@gmail.com>
2026-06-30 17:41:53 +0100

    Remove trait

--- src/core/central_disk_logger/mod.rs
+++ src/core/central_disk_logger/mod.rs
@@ -1,11 +1,9 @@
 pub mod errors;
 pub mod interface;
 pub mod task;
-pub mod traits;
 
 #[cfg(test)]
 pub mod testing;
 
 pub use interface::{DiskLoggerMessage, DiskLoggerRegistry, LoggerHandle, LoggerTaskID};
 pub use task::CentralDiskLogger;
-pub use traits::LogToDisk;
